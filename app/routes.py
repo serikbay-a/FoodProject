929a7753d199327974f9f0943db416bb8d7db3f2
Vlad Prosekov <vladprosekov2004@gmail.com>
2020-07-02 23:59:02 +0600

    made catalog index page

--- app/routes.py
+++ app/routes.py
@@ -6,5 +6,9 @@ from flask import render_template, redirect
 
 def hello():
     title = 'Food-site-title'
-    return render_template('index.html', title = title, active='main'.lower())
+    return render_template('index.html', title = title, active='catalog'.lower())
 
+@app.route('/select/')
+def select():
+    title = 'Рецепт по ингредиентам'
+    return render_template('select.html', title = title, active='select'.lower())
