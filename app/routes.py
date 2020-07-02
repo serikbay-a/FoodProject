b382cfc738914fb6525f3b58be21ce1eb16f1db4
xan.jr <63220467+serikbay-a@users.noreply.github.com>
2020-07-03 02:06:32 +0600

    minor changes

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,22 @@
 from app import app
-from flask import render_template, redirect
+from flask import render_template, redirect, url_for
+
 
 @app.route('/')
 @app.route('/index')
-
-def hello():
-    title = 'Food-site-title'
-    return render_template('index.html', title = title, active='catalog'.lower())
+@app.route('/catalog')
+def catalog():
+    title = 'имя-сайта'
+    return render_template('catalog.html', title = title, active='catalog'.lower())
+    
 
 @app.route('/select/')
 def select():
     title = 'Рецепт по ингредиентам'
     return render_template('select.html', title = title, active='select'.lower())
+
+
+@app.route('/donate/')
+def donate():
+    title = 'Поддержать проект'
+    return render_template('donate.html', title = title, active='donate'.lower())
