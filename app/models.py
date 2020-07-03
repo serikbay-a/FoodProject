bfd3cbece464cc839c6db48f1a4c71c5904733f5
xan.jr <63220467+serikbay-a@users.noreply.github.com>
2020-07-03 16:14:00 +0600

    2

--- app/models.py
+++ app/models.py
@@ -3,13 +3,14 @@ from app import db
 
 class Recepie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    ingredients = db.Column(db.String(255), nullable=False)
+    name = db.Column(db.String(255), nullable=False)
     ingredients = db.Column(db.String(1000), nullable=False)
     URL_recepie = db.Column(db.String(500), nullable=False)
     category_food = db.Column(db.String(100), nullable=False)
 
 def __init__(self, name, city, addr,pin):
+   self.id = id
    self.name = name
-   self.city = city
-   self.addr = addr
-   self.pin = pin
+   self.ingredients = ingredients
+   self.URL_recepie = URL_recepie
+   self.category_food = category_food
