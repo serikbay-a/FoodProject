8c6f0ba5ba8564b5cab2c1415106b33ec6875f58
xan.jr <63220467+serikbay-a@users.noreply.github.com>
2020-07-07 11:46:49 +0600

    вот

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,5 @@ from wtforms.validators import DataRequired
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 class MyForm(FlaskForm):
-    name = StringField('name of image', validators=[DataRequired()])
-    file = FileField('image file', validators=[ FileRequired(), FileAllowed(['png', 'jpeg', 'svg'], 'IMG file only!')])
     submit = SubmitField('Добавить')
     
--- app/routes.py
+++ app/routes.py
@@ -9,55 +9,58 @@ from app import db
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    recipe = db.Column(db.String(1000), nullable=False)
-    url = db.Column(db.String(500), nullable=False)
-    category = db.Column(db.String(100))
-    time = db.Column(db.String(255))
+    ingridients = db.Column(db.String(2000), nullable=False)
+    url = db.Column(db.String(100), nullable=False)
+    url_img = db.Column(db.String(1000))
+    category = db.Column(db.String(100), nullable=False)
+    time = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return '<name: {} ingridients: {} url: {} url_img: {} category: {} time: {}>'.format(self.name,self.ingridients,self.url,self.url_img,self.category,self.time)
 
 @app.route('/')
 @app.route('/index')
 @app.route('/catalog')
 def catalog():
     title = 'имя-сайта'
-    cards = [{'title':'Курочка с хреном', 'pic':'https://img1.russianfood.com/dycontent/images_upl/185/big_184766.jpg', 'url': '', 'recipe':['курочка', "хрен", "майонезик", "соус"]}]
+    cards = Recipe.query.all()
     return render_template('catalog.html', title = title, active='catalog'.lower(), cards = cards)
     
 
-@app.route('/select/')
+@app.route('/select')
 def select():
     title = 'Рецепт по ингредиентам'
     return render_template('select.html', title = title, active='select'.lower())
 
 
-@app.route('/donate/')
+@app.route('/donate')
 def donate():
     title = 'Поддержать проект'
     return render_template('donate.html', title = title, active='donate'.lower())
 
-@app.route('/add/', methods=('GET', 'POST'))
+@app.route('/add', methods=('GET', 'POST'))
 def add():
     form = MyForm()
     if form.validate_on_submit():
-        _, file_ext = os.path.splitext(form.file.data.filename)
-        files_dir= os.path.join(os.getcwd(), 'files')
-        fname=form.name.data+file_ext
-        filename=os.path.join(files_dir, fname)
-        if 'files' not in os.listdir(os.getcwd()):
-            os.mkdir(files_dir)
-        form.file.data.save(filename)
 
-        name_of_dish = request.form['name_of_dish']
+        name = request.form['name']
         ingridients = request.form['ingridients']
         url = request.form['url']
+        url_img = request.form['url_img']
         category = request.form['category']
         time = request.form['time']
 
-        recipe = Recipe(name=name_of_dish,recipe=ingridients,url=url,category=category,time=time)
+        recipe = Recipe(name=name,
+                        ingridients=ingridients,
+                        url=url,
+                        url_img=url_img,
+                        category=category,
+                        time=time)
+        print(recipe)
 
-        try: 
-            db.session.add(recipe)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'Error!!!'
+        
+        db.session.add(recipe)
+        db.session.commit()
+           
+    
     return render_template('add.html', active='donate'.lower(), form = form)
