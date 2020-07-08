10dfa96d3a63e550f6c91851eee09f65033b9843
Vlad Prosekov <vladprosekov2004@gmail.com>
2020-07-08 23:49:09 +0600

    upgraded catalog.css for multiple cards

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
         PORT = int(os.environ.get('SERVER_PORT', '5555'))
     except ValueError:
         PORT = 5555
-    app.run(HOST, PORT, debug = False)
+    app.run(HOST, PORT, debug = True)
